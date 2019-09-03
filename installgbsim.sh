7b8bdda91b99add0fd42d0ab6cb3c9d87c19ab55
Vaishnav M A <mavaishnav007@gmail.com>
2019-09-03 22:25:53 +0530

    Update installgbsim.sh

--- installgbsim.sh
+++ installgbsim.sh
@@ -25,7 +25,7 @@ make
 make install
 git clone https://gist.github.com/vaishnav98/c084ebd8bc0531e714baf2413770b5c3 dummy_hcd
 cd dummy_hcd
-wget https://raw.githubusercontent.com/beagleboard/vaishnav98/4.14/drivers/usb/gadget/udc/dummy_hcd.c
+wget https://raw.githubusercontent.com/vaishnav98/linux/4.14/drivers/usb/gadget/udc/dummy_hcd.c
 make 
 cp dummy_hcd.ko /lib/modules/`uname -r`/kernel/drivers/usb/gadget/legacy/
 depmod
@@ -36,11 +36,17 @@ systemctl daemon-reload
 if [ "x${board}" = "xTI_AM335x_PocketBeagle" ] ; then
     systemctl enable gbsim@1
     systemctl enable gbsim@2
+    systemctl start gbsim@1
+    systemctl start gbsim@2    
 else
     systemctl enable gbsim@1
     systemctl enable gbsim@2
     systemctl enable gbsim@3
     systemctl enable gbsim@4
+    systemctl start gbsim@1
+    systemctl start gbsim@2
+    systemctl start gbsim@3
+    systemctl start gbsim@4
 fi
 cd ..
 
